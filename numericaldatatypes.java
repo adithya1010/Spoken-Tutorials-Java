cf99570aa1cacda0da15090259c40f2c09bfbd26
Adithya S.T <57008821+adithya1010@users.noreply.github.com>
2022-01-10 22:29:19 +0530

    Spoken Tutorials-numerical-datatypes

---
+++ numericaldatatypes.java
@@ -0,0 +1,10 @@
+public class numericaldatatypes
+{
+  public static void main(String args[])
+  {
+    int distance=98769056;
+    float distance_float=23.567843f;
+    System.out.println(distance);
+    System.out.println(distance_float);
+  }
+}
