298241cf34054f502af7569bbd4e371445fd9a78
Adithya S.T <57008821+adithya1010@users.noreply.github.com>
2022-01-12 21:51:33 +0530

    arithmeticoperations-java

---
+++ arithemeticoperations.java
@@ -0,0 +1,10 @@
+public class arithemeticoperations {
+
+  public static void main(String[] args) {
+    int x=75;
+    float y=10f;
+    float result;
+    result=x/y;
+    System.out.println(result);
+  }
+}
