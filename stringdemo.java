64d35cab79bcd13f547d123e81c608f02d0d1e4a
Adithya S.T <57008821+adithya1010@users.noreply.github.com>
2022-01-13 19:35:26 +0530

    stringdemo-java

---
+++ stringdemo.java
@@ -0,0 +1,9 @@
+class stringdemo{
+  public static void main(String[] args) {
+    String greet="Hello";
+    String name="Java";
+    char SPACE=' ';
+    String msg=greet+SPACE+name;
+    System.out.println(msg);
+  }
+}
