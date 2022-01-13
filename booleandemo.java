b2fde16292daddda92a10da1d131f09af99614c4
Adithya S.T <57008821+adithya1010@users.noreply.github.com>
2022-01-13 20:43:35 +0530

    "boolean-java"

---
+++ booleandemo.java
@@ -0,0 +1,10 @@
+public class booleandemo {
+public static void main(String[] args) {
+  boolean b,c;
+  int weight=45;
+  b=weight>=40;
+  c=weight!=40;
+  System.out.println(b);
+  System.out.println(c);
+}
+}
