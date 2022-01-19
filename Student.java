0a653f5f2ffcde2e8f9fb7841732b01a5c571e63
Adithya S.T <57008821+adithya1010@users.noreply.github.com>
2022-01-19 11:26:54 +0530

    rev-creatingclasses

--- Student.java
+++ Student.java
@@ -1,10 +1,10 @@
 public class Student{
-   int roll_no;
+   int roll_no=10;
   String name="Adithya";
 
 public void studentDetail()
   {
-    System.out.println("The roll no is"+roll_no);
-    System.out.println("The name is"+name);
+    System.out.println("The roll no is "+roll_no);
+    System.out.println("The name is "+name);
   }
 }
--- creatingclasses.java
+++ creatingclasses.java
@@ -2,8 +2,8 @@ import java.io.*;
 import java.util.*;
 public class creatingclasses{
   public static void main(String[] args) {
-    System.out.println("We have created a class with two variables and one method");;
-    Student s=new Student();
-    s.studentDetail();
+  System.out.println("We have created a class with two variables and one method");;
+  Student s=new Student();//classname objectname=new classname();
+  s.studentDetail();  //objectname.functionname();
 }
 }
